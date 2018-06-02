712a813179ddc0ba4b6db954da5c3243a6f8c40a
iampeterbanjo <peterbanjo@gmail.com>
2018-06-02 19:44:57 +0100

    configure aws amplify

---
+++ app/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "tengen-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://9gg2oi6z9a.execute-api.eu-west-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_gDVrXnwxs",
+    APP_CLIENT_ID: "9gg2oi6z9a",
+    IDENTITY_POOL_ID: "eu-west-1:5705d4db-9d0d-4a48-996b-58b78a2b0f6f",
+  },
+};
