e8406c43473dcf57da78b0c00239c1ffc262a377
iampeterbanjo <peterbanjo@gmail.com>
2018-06-02 20:39:22 +0100

    login with aws cognito

--- app/src/config.js
+++ app/src/config.js
@@ -10,7 +10,7 @@ export default {
   cognito: {
     REGION: "eu-west-1",
     USER_POOL_ID: "eu-west-1_gDVrXnwxs",
-    APP_CLIENT_ID: "9gg2oi6z9a",
+    APP_CLIENT_ID: "jj6rgk94u071luh0opj7nd03",
     IDENTITY_POOL_ID: "eu-west-1:5705d4db-9d0d-4a48-996b-58b78a2b0f6f",
   },
 };
